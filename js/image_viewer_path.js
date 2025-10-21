c05048e7b47b48899b0a62b78087c5d219fa6a38
CJLuciano <christopherluciano@gmail.com>
2025-10-20 21:24:29 -0400

    formatting

--- js/image_viewer_path.js
+++ js/image_viewer_path.js
@@ -225,7 +225,6 @@ const contextMenuPath = {
         `;
     },
 
-    // CHANGED: This function now captures the full path
     _handleRightClick(e) {
         if (!this.enabled) return;
         this.lastClickedPath = null;
